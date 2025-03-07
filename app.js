0b1fe10ffe873368f198663e1ec90e1d1b8a6c2b
Ardiyansah <ardihafiedz@gmail.com>
2025-03-07 15:23:18 +0700

    Pisahkan kode logika dan DOM untuk memperbaiki error testing

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-function add(a, b) {
-    return a + b;
-  }
-  
-  // Contoh penggunaan
-  document.getElementById('output').textContent = `2 + 3 = ${add(2, 3)}`;
+const { add } = require('./math');  // Import fungsi dari math.js
+
+document.getElementById('output').textContent = `2 + 3 = ${add(2, 3)}`;
--- app.test.js
+++ app.test.js
@@ -1,4 +1,4 @@
-const { add } = require('./app');
+const { add } = require('./math');  // Test file math.js, bukan app.js
 
 test('menambahkan 2 + 3 harus menghasilkan 5', () => {
   expect(add(2, 3)).toBe(5);
